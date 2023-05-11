ebdf0fd467bc2109c4f9c4da597665e7e8c04443
Elman Steve Laguna <32938596+salsasteve@users.noreply.github.com>
2023-05-11 02:26:18 +0000

    database connection create to neondb

--- ui/apps/authentication/models.py
+++ ui/apps/authentication/models.py
@@ -9,9 +9,9 @@ from apps import db, login_manager
 
 from apps.authentication.util import hash_pass
 
-class Users(db.Model, UserMixin):
 
-    __tablename__ = 'Users'
+class Users(db.Model, UserMixin):
+    __tablename__ = "Users"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
@@ -23,11 +23,11 @@ class Users(db.Model, UserMixin):
             # depending on whether value is an iterable or not, we must
             # unpack it's value (when **kwargs is request.form, some values
             # will be a 1-element list)
-            if hasattr(value, '__iter__') and not isinstance(value, str):
+            if hasattr(value, "__iter__") and not isinstance(value, str):
                 # the ,= unpack of a singleton fails PEP8 (travis flake8 test)
                 value = value[0]
 
-            if property == 'password':
+            if property == "password":
                 value = hash_pass(value)  # we need bytes here (not plain str)
 
             setattr(self, property, value)
@@ -43,6 +43,6 @@ def user_loader(id):
 
 @login_manager.request_loader
 def request_loader(request):
-    username = request.form.get('username')
+    username = request.form.get("username")
     user = Users.query.filter_by(username=username).first()
     return user if user else None
